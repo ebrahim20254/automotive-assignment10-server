52725482f2f85f7334f35b1233c2a0f9478e228f
ebrahim20254 <ebrahimkhalil125800@gmail.com>
2023-10-23 23:15:35 +0600

    deleted car product done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.Port || 5000;
 
@@ -46,6 +46,13 @@ async function run() {
     const newCart  = req.body;
     console.log(newCart);
     const result = await cartCollection.insertOne(newCart);
+    res.send(result);
+   })
+
+   app.delete('/cart/:id', async(req, res) =>{
+    const id = req.params.id;
+    const query = {_id : new ObjectId (id)}
+    const result = await cartCollection.deleteOne(query);
     res.send(result);
    })
 
